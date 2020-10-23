7859f1ce79cd301a280cc3ea9e31e792ef43dfbf
martibook <martibook@outlook.com>
2020-10-23 23:54:19 +0800

    [quicksort]

--- chapter7/quicksort/src/Task.kt
+++ chapter7/quicksort/src/Task.kt
@@ -2,6 +2,9 @@
 
 package chapter7.quicksort.src
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun swap(A: IntArray, a: Int, b: Int) {
     val t = A[a]
     A[a] = A[b]
@@ -28,18 +31,20 @@ fun enhancedPartition(A: IntArray, p: Int, r: Int): Pair<Int, Int> {
     var i = p - 1 // <x 的最后下标
     var k = i // =x 的最后下标
     for (j in p until r) {
-        if (A[j] < x) {
-            i += 1
-            swap(A, i, j) // 被换到 A[j] 的 A[i] 属于[i+1,k]区间，==x
-            k += 1
-            swap(A, k, j) // 被置换后的 A[j]==x, 需被换到 [i+1,k]区间
-        }
-        if (A[j] == x) {
-            k += 1
-            swap(A, k, j)
-        }
-        if (A[j] > x) {
-            // do nothing
+        when {
+            (A[j] < x) -> {
+                i += 1
+                swap(A, i, j) // 被换到 A[j] 的 A[i] 属于[i+1,k]区间，==x
+                k += 1
+                swap(A, k, j) // 被置换后的 A[j]==x, 需被换到 [i+1,k]区间
+            }
+            (A[j] == x) -> {
+                k += 1
+                swap(A, k, j)
+            }
+            (A[j] > x) -> {
+                // do nothing
+            }
         }
     }
     k += 1
@@ -57,9 +62,56 @@ fun basicQuicksort(A: IntArray, p: Int, r: Int) {
 
 fun enhancedQuicksort(A: IntArray, p: Int, r: Int) {
     if (p<r) {
-        val pair = enhancedPartition(A, p, r)
-        enhancedQuicksort(A, p, pair.first-1)
-        enhancedQuicksort(A, pair.second+1, r)
+        val (i, k) = enhancedPartition(A, p, r)
+        enhancedQuicksort(A, p, i-1)
+        enhancedQuicksort(A, k+1, r)
+    }
+}
+
+fun swap(A: Array<Pair<Int, Int>>, i:Int, j:Int) {
+    val t = A[i]
+    A[i] = A[j]
+    A[j] = t
+}
+
+fun enhancedFuzzyPartition(A: Array<Pair<Int, Int>>, p: Int, r: Int): Pair<Int, Int> {
+    // pick a random one, swap with A[r]
+    var x = A[r]
+    var i = p -1  // <x 的最后下标
+    var k = i // =x 的最后下标
+    for ( j in p until r) {
+        when {
+            (A[j].second < x.first) -> {
+                // A[j] < x
+                i += 1
+                swap(A, i, j)
+                k += 1
+                swap(A, k, j)
+            }
+            (A[j].first > x.second) -> {
+                // A[j] > x
+                // do nothing
+            }
+            (A[j].second >= x.first && A[j].first <= x.second) -> {
+                // intersection 想象 A[j] 平移去交叉 x, 在有交集的整个过程中，上式都成立
+                k += 1
+                swap(A, k, j)
+
+                // 此时取 intersection 部分作为新的 x, 因为后续的 A[j] 只有和 intersection 有交集，这些区间才可以被看成等价
+                x = Pair(max(x.first, A[j].first), min(x.second, A[j].second))
+            }
+        }
+    }
+    k += 1
+    swap(A, k, r)
+    return Pair(i+1, k)
+}
+
+fun enhancedFuzzyQuicksort(A: Array<Pair<Int, Int>>, p: Int, r: Int) {
+    if (p<r) {
+        val (i, k) = enhancedFuzzyPartition(A, p, r)
+        enhancedFuzzyQuicksort(A, p, i-1)
+        enhancedFuzzyQuicksort(A, k+1, r)
     }
 }
 
@@ -79,6 +131,11 @@ fun main() {
 
     enhancedQuicksort(B, 0, B.size-1)
     println(B.joinToString(" "))
+
+    val F = arrayOf(Pair(4,6), Pair(1,2), Pair(3,5), Pair(6,7), Pair(6,9))
+    println(F.joinToString(" "))
+    enhancedFuzzyQuicksort(F, 0, F.size-1)
+    println(F.joinToString(" "))
 }
 
 
